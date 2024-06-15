0478a44311f24b439b918f1f1a902af327e7af88
hou42644 <165400844+hou42644@users.noreply.github.com>
2024-06-15 12:53:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,13 +4,13 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
+let userID = 'db457919-b36d-4b8c-b0e6-a19d962e3560';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org, workers.cloudflare.cyou'
 
-let sub = '';// 留空则使用内置订阅
-let subconverter = 'url.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。自带虚假uuid和host订阅。
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
+let sub = 'ss.song.dns-dynamic.net';// 留空则使用内置订阅
+let subconverter = 'o.yy3434.cloudns.biz';// clash订阅转换后端，目前使用肥羊的订阅转换功能。自带虚假uuid和host订阅。
+let subconfig = "https://raw.githubusercontent.com/hou42644/edgetunnel/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
